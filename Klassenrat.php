605a76d12d6b1b8cae2328c768b47b1014bdbcfd
Ilativ <derIlativ@gmail.com>
2019-07-28 17:58:50 +0200

    lul

---
+++ Klassenrat.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="style/sbar.css">
+  <link rel="stylesheet" href="style/Klassenrat.css">
+  <title>Klassenrat</title>
+</head>
+<body>
+  <div id= "se">
+      <a id= "se_t" href="Startseite.php">Startseite</a>
+      <a id= "se_t" href="Stundenplan.php">Stundenplan</a>
+      <a id= "se_t" href="Vertretungsplan.php">Vertretungsplan</a>
+      <a id= "se_t" href="KurseXFächer.php">Kurse/Fächer</a>
+      <a id= "se_t" href="Klassenrat.php">Klassenrat</a>
+      <a id= "se_t" href="News.php">News</a>
+      <a id= "se_t" href="Termine.php">Termine</a>
+      <div id="logouut"><a id= "logoutt" href="#" >Logout</a><div>
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Einloggen</title>
+    <link rel="stylesheet" href="style/lo.css">
+  </head>
+  <body>
+    <form class="box" action="Startseite.php" method="get">
+      <h1>Login</h1>
+      <input type="text" name="benutzername" placeholder="Benutzername">
+      <input type="password" name="password" placeholder="Passwort">
+      <input type="submit" value="Einloggen">
+    </form>
+  </body>
+</html>
